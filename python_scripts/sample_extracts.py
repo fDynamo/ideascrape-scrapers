b0e36d06f5777fa13aa16e52ce1c5719ca21fa1d
fDynamo <fontaine.dynamo@gmail.com>
2024-01-23 00:45:12 -0800

    sample extracts

---
+++ python_scripts/sample_extracts.py
@@ -0,0 +1,26 @@
+# Read both csv files
+# Select 5000 random ones
+# Write to new file
+
+import os.path as path
+import pandas as pd
+
+dir_path = path.dirname(path.realpath(__file__))
+MVP_OUT_FOLDER = path.join(dir_path, "out", "mvp")
+
+aift_extract = path.join(MVP_OUT_FOLDER, "aift_extract.csv")
+aift_sample_out = path.join(MVP_OUT_FOLDER, "aift_sample.csv")
+
+ph_extract = path.join(MVP_OUT_FOLDER, "ph_extract.csv")
+ph_sample_out = path.join(MVP_OUT_FOLDER, "ph_sample.csv")
+
+# Sample
+COUNT_SAMPLED_ROWS = 1000
+aift_df = pd.read_csv(aift_extract)
+aift_sample = aift_df.sample(n=COUNT_SAMPLED_ROWS)
+aift_sample.to_csv(aift_sample_out, index=False, encoding="utf-8")
+
+COUNT_SAMPLED_ROWS = 1000
+ph_df = pd.read_csv(ph_extract)
+ph_sample = ph_df.sample(n=COUNT_SAMPLED_ROWS)
+ph_sample.to_csv(ph_sample_out, index=False, encoding="utf-8")
