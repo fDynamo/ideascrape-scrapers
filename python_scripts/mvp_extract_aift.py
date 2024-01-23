690b69a6b4c39aca9b2b5138b2a2dbe7a50b9348
fDynamo <fontaine.dynamo@gmail.com>
2024-01-22 22:26:41 -0800

    minor

--- python_scripts/mvp_extract_aift.py
+++ python_scripts/mvp_extract_aift.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os.path as path
 from os import listdir
 from calculate_unique_url import calculate_unique_url
+from helpers.string_formatters import camel_to_snake_case
 
 dir_path = path.dirname(path.realpath(__file__))
 AIFT_OUT_PATH = path.join(dir_path, "..", "scrape_aift", "out")
@@ -26,13 +27,15 @@ for period_file in period_files:
 
 # Merge dfs
 period_df: pd.DataFrame = pd.concat(period_df_list, axis=0)
-period_df = period_df.rename(columns={"starRatings": "rating"})
-period_df = period_df.drop_duplicates(subset="postUrl", keep="last").reset_index(
+new_columns = [camel_to_snake_case(col) for col in period_df.columns]
+period_df.columns = new_columns
+period_df = period_df.rename(columns={"star_ratings": "rating"})
+period_df = period_df.drop_duplicates(subset="post_url", keep="last").reset_index(
     drop=True
 )
 
 # Save post urls for comparison later
-all_post_urls = period_df[["postUrl"]]
+all_post_urls = period_df[["post_url"]]
 
 
 # Read from posts
@@ -57,11 +60,11 @@ for post_file in post_files:
     post_df_list.append(df)
 
 post_df = pd.concat(post_df_list, axis=0)
-post_df.columns = ["postUrl", "countRatings", "description", "launchDateText"]
-post_df = post_df.drop_duplicates(subset="postUrl", keep="last").reset_index(drop=True)
+post_df.columns = ["post_url", "count_ratings", "description", "launch_date_text"]
+post_df = post_df.drop_duplicates(subset="post_url", keep="last").reset_index(drop=True)
 
 
-aift_df = post_df.merge(period_df, how="inner", on="postUrl").reset_index(drop=True)
+aift_df = post_df.merge(period_df, how="inner", on="post_url").reset_index(drop=True)
 
 
 def fix_launch_date(launch_date: str):
@@ -72,30 +75,32 @@ def fix_launch_date(launch_date: str):
     return launch_date
 
 
-aift_df["launchDateText"] = aift_df["launchDateText"].apply(fix_launch_date)
+aift_df["launch_date_text"] = aift_df["launch_date_text"].apply(fix_launch_date)
 
 # Grab date from featured at
-aift_df["launchDate"] = pd.to_datetime(aift_df["launchDateText"], format="mixed")
+aift_df["launch_date"] = pd.to_datetime(aift_df["launch_date_text"], format="mixed")
 
 # Get unique url
-aift_df["uniqueUrl"] = aift_df["sourceUrl"].apply(calculate_unique_url)
+aift_df["unique_url"] = aift_df["source_url"].apply(calculate_unique_url)
 
 
 # Reorder
 aift_df = aift_df[
     [
-        "postUrl",
-        "sourceUrl",
-        "uniqueUrl",
-        "countSaves",
-        "countRatings",
+        "post_url",
+        "source_url",
+        "unique_url",
+        "count_saves",
+        "count_ratings",
         "rating",
-        "projectName",
+        "project_name",
         "description",
-        "launchDate",
+        "launch_date",
     ]
 ]
+
 aift_df.index.name = "index"
+
 print(aift_df)
 print("Columns")
 print(aift_df.columns)
@@ -103,8 +108,10 @@ print(aift_df.columns)
 aift_df.to_csv(OUT_FILE, encoding="utf-8")
 
 # Find out unscraped urls
-unscraped_df = all_post_urls.merge(aift_df, on="postUrl", indicator=True, how="left")
+unscraped_df = all_post_urls.merge(
+    aift_df, left_on="post_url", right_on="post_url", indicator=True, how="left"
+)
 unscraped_df = unscraped_df[unscraped_df["_merge"] == "left_only"].reset_index(
     drop=True
-)[["postUrl"]]
+)[["post_url"]]
 unscraped_df.to_csv(UNSCRAPED_OUT, encoding="utf-8")
